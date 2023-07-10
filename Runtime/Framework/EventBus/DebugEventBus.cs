e27112633053e4debd624bceaa49f0101fa09f80
DiscreteTom <discrete_tom@outlook.com>
2023-07-10 22:17:31 +0800

    chore: optimize code

--- Runtime/Framework/EventBus/DebugEventBus.cs
+++ Runtime/Framework/EventBus/DebugEventBus.cs
@@ -24,110 +24,115 @@ namespace DT.UniStart {
       this.bus = bus;
     }
 
+    bool isAddListenerModeEnabled => (this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener;
+    bool isRemoveListenerModeEnabled => (this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener;
+    bool isInvokeModeEnabled => (this.mode & DebugEventBusMode.Invoke) == DebugEventBusMode.Invoke;
+    bool isParameterModeEnabled => (this.mode & DebugEventBusMode.Parameter) == DebugEventBusMode.Parameter;
+
     public UnityAction AddListener(T key, UnityAction action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddListener: key = {key}");
       return this.bus.AddListener(key, action);
     }
     public UnityAction AddOnceListener(T key, UnityAction action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
       return this.bus.AddOnceListener(key, action);
     }
     public UnityAction<T0> AddListener<T0>(T key, UnityAction<T0> action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddListener: key = {key}");
       return this.bus.AddListener(key, action);
     }
     public UnityAction<T0> AddOnceListener<T0>(T key, UnityAction<T0> action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
       return this.bus.AddOnceListener(key, action);
     }
     public UnityAction<T0, T1> AddListener<T0, T1>(T key, UnityAction<T0, T1> action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddListener: key = {key}");
       return this.bus.AddListener(key, action);
     }
     public UnityAction<T0, T1> AddOnceListener<T0, T1>(T key, UnityAction<T0, T1> action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
       return this.bus.AddOnceListener(key, action);
     }
     public UnityAction<T0, T1, T2> AddListener<T0, T1, T2>(T key, UnityAction<T0, T1, T2> action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddListener: key = {key}");
       return this.bus.AddListener(key, action);
     }
     public UnityAction<T0, T1, T2> AddOnceListener<T0, T1, T2>(T key, UnityAction<T0, T1, T2> action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
       return this.bus.AddOnceListener(key, action);
     }
     public UnityAction<T0, T1, T2, T3> AddListener<T0, T1, T2, T3>(T key, UnityAction<T0, T1, T2, T3> action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddListener: key = {key}");
       return this.bus.AddListener(key, action);
     }
     public UnityAction<T0, T1, T2, T3> AddOnceListener<T0, T1, T2, T3>(T key, UnityAction<T0, T1, T2, T3> action) {
-      if ((this.mode & DebugEventBusMode.AddListener) == DebugEventBusMode.AddListener) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
+      if (this.isAddListenerModeEnabled) Debug.Log($"DebugEventBus.AddOnceListener: key = {key}");
       return this.bus.AddOnceListener(key, action);
     }
 
     public UnityAction RemoveListener(T key, UnityAction action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
       return this.bus.RemoveListener(key, action);
     }
     public UnityAction RemoveOnceListener(T key, UnityAction action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
       return this.bus.RemoveOnceListener(key, action);
     }
     public UnityAction<T0> RemoveListener<T0>(T key, UnityAction<T0> action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
       return this.bus.RemoveListener(key, action);
     }
     public UnityAction<T0> RemoveOnceListener<T0>(T key, UnityAction<T0> action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
       return this.bus.RemoveOnceListener(key, action);
     }
     public UnityAction<T0, T1> RemoveListener<T0, T1>(T key, UnityAction<T0, T1> action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
       return this.bus.RemoveListener(key, action);
     }
     public UnityAction<T0, T1> RemoveOnceListener<T0, T1>(T key, UnityAction<T0, T1> action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
       return this.bus.RemoveOnceListener(key, action);
     }
     public UnityAction<T0, T1, T2> RemoveListener<T0, T1, T2>(T key, UnityAction<T0, T1, T2> action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
       return this.bus.RemoveListener(key, action);
     }
     public UnityAction<T0, T1, T2> RemoveOnceListener<T0, T1, T2>(T key, UnityAction<T0, T1, T2> action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
       return this.bus.RemoveOnceListener(key, action);
     }
     public UnityAction<T0, T1, T2, T3> RemoveListener<T0, T1, T2, T3>(T key, UnityAction<T0, T1, T2, T3> action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveListener: key = {key}");
       return this.bus.RemoveListener(key, action);
     }
     public UnityAction<T0, T1, T2, T3> RemoveOnceListener<T0, T1, T2, T3>(T key, UnityAction<T0, T1, T2, T3> action) {
-      if ((this.mode & DebugEventBusMode.RemoveListener) == DebugEventBusMode.RemoveListener) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
+      if (this.isRemoveListenerModeEnabled) Debug.Log($"DebugEventBus.RemoveOnceListener: key = {key}");
       return this.bus.RemoveOnceListener(key, action);
     }
 
     public void Invoke(T key) {
-      if ((this.mode & DebugEventBusMode.Invoke) == DebugEventBusMode.Invoke) Debug.Log($"DebugEventBus.Invoke: key = {key}");
+      if (this.isInvokeModeEnabled) Debug.Log($"DebugEventBus.Invoke: key = {key}");
       this.bus.Invoke(key);
     }
     public void Invoke<T0>(T key, T0 arg0) {
-      if ((this.mode & DebugEventBusMode.Invoke) == DebugEventBusMode.Invoke) Debug.Log($"DebugEventBus.Invoke: key = {key}");
-      if ((this.mode & DebugEventBusMode.Parameter) == DebugEventBusMode.Parameter) Debug.Log($"DebugEventBus.Invoke: arg0 = {arg0}");
+      if (this.isInvokeModeEnabled) Debug.Log($"DebugEventBus.Invoke: key = {key}");
+      if (this.isParameterModeEnabled) Debug.Log($"DebugEventBus.Invoke: arg0 = {arg0}");
       this.bus.Invoke(key, arg0);
     }
     public void Invoke<T0, T1>(T key, T0 arg0, T1 arg1) {
-      if ((this.mode & DebugEventBusMode.Invoke) == DebugEventBusMode.Invoke) Debug.Log($"DebugEventBus.Invoke: key = {key}");
-      if ((this.mode & DebugEventBusMode.Parameter) == DebugEventBusMode.Parameter) Debug.Log($"DebugEventBus.Invoke: arg0 = {arg0}, arg1 = {arg1}");
+      if (this.isInvokeModeEnabled) Debug.Log($"DebugEventBus.Invoke: key = {key}");
+      if (this.isParameterModeEnabled) Debug.Log($"DebugEventBus.Invoke: arg0 = {arg0}, arg1 = {arg1}");
       this.bus.Invoke(key, arg0, arg1);
     }
     public void Invoke<T0, T1, T2>(T key, T0 arg0, T1 arg1, T2 arg2) {
-      if ((this.mode & DebugEventBusMode.Invoke) == DebugEventBusMode.Invoke) Debug.Log($"DebugEventBus.Invoke: key = {key}");
-      if ((this.mode & DebugEventBusMode.Parameter) == DebugEventBusMode.Parameter) Debug.Log($"DebugEventBus.Invoke: arg0 = {arg0}, arg1 = {arg1}, arg2 = {arg2}");
+      if (this.isInvokeModeEnabled) Debug.Log($"DebugEventBus.Invoke: key = {key}");
+      if (this.isParameterModeEnabled) Debug.Log($"DebugEventBus.Invoke: arg0 = {arg0}, arg1 = {arg1}, arg2 = {arg2}");
       this.bus.Invoke(key, arg0, arg1, arg2);
     }
     public void Invoke<T0, T1, T2, T3>(T key, T0 arg0, T1 arg1, T2 arg2, T3 arg3) {
-      if ((this.mode & DebugEventBusMode.Invoke) == DebugEventBusMode.Invoke) Debug.Log($"DebugEventBus.Invoke: key = {key}");
-      if ((this.mode & DebugEventBusMode.Parameter) == DebugEventBusMode.Parameter) Debug.Log($"DebugEventBus.Invoke: arg0 = {arg0}, arg1 = {arg1}, arg2 = {arg2}, arg3 = {arg3}");
+      if (this.isInvokeModeEnabled) Debug.Log($"DebugEventBus.Invoke: key = {key}");
+      if (this.isParameterModeEnabled) Debug.Log($"DebugEventBus.Invoke: arg0 = {arg0}, arg1 = {arg1}, arg2 = {arg2}, arg3 = {arg3}");
       this.bus.Invoke(key, arg0, arg1, arg2, arg3);
     }
   }
