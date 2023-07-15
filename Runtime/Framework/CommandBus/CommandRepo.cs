862e5b17c09b7befe645d2b830f9e87dc8fa74fb
DiscreteTom <discrete_tom@outlook.com>
2023-07-15 21:51:23 +0800

    chore: simplify CommandRepo with extension

--- Runtime/Framework/CommandBus/CommandRepo.cs
+++ Runtime/Framework/CommandBus/CommandRepo.cs
@@ -14,19 +14,10 @@ namespace DT.UniStart {
       this.bus.AddListener<T>(command);
       return this;
     }
-    public ICommandRepo Add<T>(out UnityAction named, UnityAction command) {
-      named = command;
-      return this.Add<T>(command);
-    }
     public ICommandRepo Add<T>(UnityAction<T> command) {
       this.bus.AddListener(command);
       return this;
     }
-    public ICommandRepo Add<T>(out UnityAction<T> named, UnityAction<T> command) {
-      named = command;
-      return this.Add(command);
-    }
     public void Invoke<T>(T arg) => this.bus.Invoke(arg);
-    public void Invoke<T>() where T : new() => this.bus.Invoke<T>();
   }
 }
--- Runtime/Framework/CommandBus/ICommandRepo.cs
+++ Runtime/Framework/CommandBus/ICommandRepo.cs
@@ -3,10 +3,19 @@ using UnityEngine.Events;
 namespace DT.UniStart {
   public interface ICommandRepo {
     ICommandRepo Add<T>(UnityAction command);
-    ICommandRepo Add<T>(out UnityAction named, UnityAction command);
     ICommandRepo Add<T>(UnityAction<T> command);
-    ICommandRepo Add<T>(out UnityAction<T> named, UnityAction<T> command);
     void Invoke<T>(T arg);
-    void Invoke<T>() where T : new();
+  }
+
+  public static class ICommandRepoExtension {
+    public static ICommandRepo Add<T>(this ICommandRepo self, out UnityAction named, UnityAction command) {
+      named = command;
+      return self.Add<T>(command);
+    }
+    public static ICommandRepo Add<T>(this ICommandRepo self, out UnityAction<T> named, UnityAction<T> command) {
+      named = command;
+      return self.Add(command);
+    }
+    public static void Invoke<T>(this ICommandRepo self) where T : new() => self.Invoke(new T());
   }
 }
