a11bff26db88aad1e7f0b271fed2d025f650c3a2
DiscreteTom <discrete_tom@outlook.com>
2023-07-27 13:42:28 +0800

    feat: TimerManager.Add auto remove timer

--- Runtime/Framework/Timer/TimerManager.cs
+++ Runtime/Framework/Timer/TimerManager.cs
@@ -22,19 +22,17 @@ namespace DT.UniStart {
       }
     }
 
-    T AddTimer<T>(T t, UnityAction cb) where T : Timer {
+    T AddTimer<T>(T t, UnityAction cb, IComposable mountTarget) where T : Timer {
       if (cb != null) t.onFinished.AddListener(cb);
       this.timers.Add(t);
+      if (mountTarget != null) mountTarget.onDestroy.AddListener(() => this.Remove(t));
       return t;
     }
 
-    public Timer Add(float duration, UnityAction cb = null) {
-      return this.AddTimer(new Timer(duration), cb);
-    }
-
-    public RepeatedTimer AddRepeated(float duration, UnityAction cb = null) {
-      return this.AddTimer(new RepeatedTimer(duration), cb);
-    }
+    public Timer Add(IComposable mountTarget, float duration, UnityAction cb = null) => this.AddTimer(new Timer(duration), cb, mountTarget);
+    public Timer Add(float duration, UnityAction cb = null) => this.Add(null, duration, cb);
+    public RepeatedTimer AddRepeated(IComposable mountTarget, float duration, UnityAction cb = null) => this.AddTimer(new RepeatedTimer(duration), cb, mountTarget);
+    public RepeatedTimer AddRepeated(float duration, UnityAction cb = null) => this.AddRepeated(null, duration, cb);
 
     public T Remove<T>(T t) where T : Timer {
       this.timers.Remove(t);
