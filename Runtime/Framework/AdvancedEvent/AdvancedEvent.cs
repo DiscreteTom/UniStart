7e8bb48c9fc649a04534ec142fcc7830cfc16687
DiscreteTom <discrete_tom@outlook.com>
2023-08-09 13:05:05 +0800

    refactor: simplify code, add BaseAdvancedEvent

--- Runtime/Framework/AdvancedEvent/AdvancedEvent.cs
+++ Runtime/Framework/AdvancedEvent/AdvancedEvent.cs
@@ -1,109 +1,81 @@
-using System.Collections.Generic;
+using System;
 using UnityEngine.Events;
 
 namespace DT.UniStart {
-  public class AdvancedEvent : IWatchable {
-    readonly UnityEvent e = new();
-    readonly UnityEvent once = new();
+  public class BaseAdvancedEvent<Event, Action> where Event : UnityEventBase, new() {
+    protected readonly Event E = new();
+    protected readonly Event Once = new();
 
-    public void AddListener(UnityAction action) => this.e.AddListener(action);
-    public void RemoveListener(UnityAction action) => this.e.RemoveListener(action);
-    public void AddOnceListener(UnityAction action) => this.once.AddListener(action);
-    public void RemoveOnceListener(UnityAction action) => this.once.RemoveListener(action);
+    readonly Action<Event, Action> addListenerFunc;
+    readonly Action<Event, Action> removeListenerFunc;
+
+    public BaseAdvancedEvent(Action<Event, Action> addListenerFunc, Action<Event, Action> removeListenerFunc) {
+      this.addListenerFunc = addListenerFunc;
+      this.removeListenerFunc = removeListenerFunc;
+    }
+
+    public void AddListener(Action action) => this.addListenerFunc.Invoke(E, action);
+    public void RemoveListener(Action action) => this.removeListenerFunc.Invoke(E, action);
+    public void AddOnceListener(Action action) => this.addListenerFunc.Invoke(Once, action);
+    public void RemoveOnceListener(Action action) => this.removeListenerFunc.Invoke(Once, action);
 
     public void RemoveAllListeners() {
-      this.e.RemoveAllListeners();
-      this.once.RemoveAllListeners();
+      this.E.RemoveAllListeners();
+      this.Once.RemoveAllListeners();
     }
+
+    protected void AfterInvoke() {
+      this.Once.RemoveAllListeners();
+    }
+  }
+
+  public class AdvancedEvent : BaseAdvancedEvent<UnityEvent, UnityAction>, IWatchable {
+    public AdvancedEvent() : base((e, a) => e.AddListener(a), (e, a) => e.RemoveListener(a)) { }
 
     public void Invoke() {
-      this.e.Invoke();
-      this.once.Invoke();
-      this.once.RemoveAllListeners();
+      this.E.Invoke();
+      this.Once.Invoke();
+      this.AfterInvoke();
     }
   }
 
-  public class AdvancedEvent<T0> : IWatchable<T0> {
-    readonly UnityEvent<T0> e = new();
-    readonly UnityEvent<T0> once = new();
+  public class AdvancedEvent<T0> : BaseAdvancedEvent<UnityEvent<T0>, UnityAction<T0>>, IWatchable<T0> {
+    public AdvancedEvent() : base((e, a) => e.AddListener(a), (e, a) => e.RemoveListener(a)) { }
 
-    public void AddListener(UnityAction<T0> action) => this.e.AddListener(action);
-    public void RemoveListener(UnityAction<T0> action) => this.e.RemoveListener(action);
-    public void AddOnceListener(UnityAction<T0> action) => this.once.AddListener(action);
-    public void RemoveOnceListener(UnityAction<T0> action) => this.once.RemoveListener(action);
-
-    public void RemoveAllListeners() {
-      this.e.RemoveAllListeners();
-      this.once.RemoveAllListeners();
-    }
-
-    public void Invoke(T0 arg0) {
-      this.e.Invoke(arg0);
-      this.once.Invoke(arg0);
-      this.once.RemoveAllListeners();
+    public void Invoke(T0 t0) {
+      this.E.Invoke(t0);
+      this.Once.Invoke(t0);
+      this.AfterInvoke();
     }
   }
 
-  public class AdvancedEvent<T0, T1> : IWatchable<T0, T1> {
-    readonly UnityEvent<T0, T1> e = new();
-    readonly UnityEvent<T0, T1> once = new();
+  public class AdvancedEvent<T0, T1> : BaseAdvancedEvent<UnityEvent<T0, T1>, UnityAction<T0, T1>>, IWatchable<T0, T1> {
+    public AdvancedEvent() : base((e, a) => e.AddListener(a), (e, a) => e.RemoveListener(a)) { }
 
-    public void AddListener(UnityAction<T0, T1> action) => this.e.AddListener(action);
-    public void RemoveListener(UnityAction<T0, T1> action) => this.e.RemoveListener(action);
-    public void AddOnceListener(UnityAction<T0, T1> action) => this.once.AddListener(action);
-    public void RemoveOnceListener(UnityAction<T0, T1> action) => this.once.RemoveListener(action);
-
-    public void RemoveAllListeners() {
-      this.e.RemoveAllListeners();
-      this.once.RemoveAllListeners();
-    }
-
-    public void Invoke(T0 arg0, T1 arg1) {
-      this.e.Invoke(arg0, arg1);
-      this.once.Invoke(arg0, arg1);
-      this.once.RemoveAllListeners();
+    public void Invoke(T0 t0, T1 t1) {
+      this.E.Invoke(t0, t1);
+      this.Once.Invoke(t0, t1);
+      this.AfterInvoke();
     }
   }
 
-  public class AdvancedEvent<T0, T1, T2> : IWatchable<T0, T1, T2> {
-    readonly UnityEvent<T0, T1, T2> e = new();
-    readonly UnityEvent<T0, T1, T2> once = new();
+  public class AdvancedEvent<T0, T1, T2> : BaseAdvancedEvent<UnityEvent<T0, T1, T2>, UnityAction<T0, T1, T2>>, IWatchable<T0, T1, T2> {
+    public AdvancedEvent() : base((e, a) => e.AddListener(a), (e, a) => e.RemoveListener(a)) { }
 
-    public void AddListener(UnityAction<T0, T1, T2> action) => this.e.AddListener(action);
-    public void RemoveListener(UnityAction<T0, T1, T2> action) => this.e.RemoveListener(action);
-    public void AddOnceListener(UnityAction<T0, T1, T2> action) => this.once.AddListener(action);
-    public void RemoveOnceListener(UnityAction<T0, T1, T2> action) => this.once.RemoveListener(action);
-
-    public void RemoveAllListeners() {
-      this.e.RemoveAllListeners();
-      this.once.RemoveAllListeners();
-    }
-
-    public void Invoke(T0 arg0, T1 arg1, T2 arg2) {
-      this.e.Invoke(arg0, arg1, arg2);
-      this.once.Invoke(arg0, arg1, arg2);
-      this.once.RemoveAllListeners();
+    public void Invoke(T0 t0, T1 t1, T2 t2) {
+      this.E.Invoke(t0, t1, t2);
+      this.Once.Invoke(t0, t1, t2);
+      this.AfterInvoke();
     }
   }
 
-  public class AdvancedEvent<T0, T1, T2, T3> : IWatchable<T0, T1, T2, T3> {
-    readonly UnityEvent<T0, T1, T2, T3> e = new();
-    readonly UnityEvent<T0, T1, T2, T3> once = new();
+  public class AdvancedEvent<T0, T1, T2, T3> : BaseAdvancedEvent<UnityEvent<T0, T1, T2, T3>, UnityAction<T0, T1, T2, T3>>, IWatchable<T0, T1, T2, T3> {
+    public AdvancedEvent() : base((e, a) => e.AddListener(a), (e, a) => e.RemoveListener(a)) { }
 
-    public void AddListener(UnityAction<T0, T1, T2, T3> action) => this.e.AddListener(action);
-    public void RemoveListener(UnityAction<T0, T1, T2, T3> action) => this.e.RemoveListener(action);
-    public void AddOnceListener(UnityAction<T0, T1, T2, T3> action) => this.once.AddListener(action);
-    public void RemoveOnceListener(UnityAction<T0, T1, T2, T3> action) => this.once.RemoveListener(action);
-
-    public void RemoveAllListeners() {
-      this.e.RemoveAllListeners();
-      this.once.RemoveAllListeners();
-    }
-
-    public void Invoke(T0 arg0, T1 arg1, T2 arg2, T3 arg3) {
-      this.e.Invoke(arg0, arg1, arg2, arg3);
-      this.once.Invoke(arg0, arg1, arg2, arg3);
-      this.once.RemoveAllListeners();
+    public void Invoke(T0 t0, T1 t1, T2 t2, T3 t3) {
+      this.E.Invoke(t0, t1, t2, t3);
+      this.Once.Invoke(t0, t1, t2, t3);
+      this.AfterInvoke();
     }
   }
 }
