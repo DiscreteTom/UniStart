4895d49b47342666f3cb79ebed50d80221675afb
DiscreteTom <discrete_tom@outlook.com>
2024-06-20 23:48:38 +0800

    chore: simplify code for EventBus

--- Runtime/Framework/EventBus/EventBus.cs
+++ Runtime/Framework/EventBus/EventBus.cs
@@ -4,11 +4,7 @@ using UnityEngine.Events;
 
 namespace DT.UniStart {
   public class EventBus : IEventBus {
-    readonly Dictionary<Type, object> dict;
-
-    public EventBus() {
-      this.dict = new Dictionary<Type, object>();
-    }
+    readonly Dictionary<Type, object> dict = new();
 
     public virtual UnityAction AddListener<T>(UnityAction listener) where T : IEvent => (this.dict.GetOrAdd(typeof(T), () => new AdvancedEvent<T>()) as AdvancedEvent<T>).AddListener(listener);
     public virtual UnityAction RemoveListener<T>(UnityAction listener) where T : IEvent => (this.dict.GetOrDefault(typeof(T)) as AdvancedEvent<T>)?.RemoveListener(listener);
