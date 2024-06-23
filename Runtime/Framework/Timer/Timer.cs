2b8e00fc4db7373f626e114a7faae53dee3d31a8
DiscreteTom <discrete_tom@outlook.com>
2024-06-23 21:09:19 +0800

    chore: optimize code for Timer

--- Runtime/Framework/Timer/Timer.cs
+++ Runtime/Framework/Timer/Timer.cs
@@ -1,22 +1,17 @@
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace DT.UniStart {
   public class Timer {
-    public float duration { get; private set; }
-    public float elapsed { get; private set; }
-    public bool stopped { get; private set; }
-    public AdvancedEvent onFinished { get; private set; }
+    public readonly float duration;
+    public float elapsed { get; private set; } = 0;
+    public bool stopped { get; private set; } = false;
+    public readonly AdvancedEvent onFinished = new();
 
     public float progress => Mathf.Clamp01(this.elapsed / this.duration);
     public bool finished => this.elapsed >= this.duration;
 
     public Timer(float duration) {
       this.duration = duration;
-      this.elapsed = 0;
-      this.stopped = false;
-      this.onFinished = new AdvancedEvent();
     }
 
     public virtual void Update(float deltaTime) {
