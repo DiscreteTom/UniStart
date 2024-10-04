c1fd333eac5487a75296440b0db98e9186ef21b9
DiscreteTom <discrete_tom@outlook.com>
2024-10-04 18:53:23 +0800

    tests: update CommandBusTest

--- Tests/Runtime/Framework/CommandBusTest.cs
+++ Tests/Runtime/Framework/CommandBusTest.cs
@@ -1,34 +1,41 @@
 using NUnit.Framework;
 using DT.UniStart;
+using System;
 
 public class CommandBusTest {
-  record E;
-  record EE(int a);
+  record E : ICommand<Watch<int>> {
+    public void Invoke(Watch<int> ctx) {
+      ctx.Value++;
+    }
+  }
+  record EE(int a) : ICommand<Watch<int>> {
+    public void Invoke(Watch<int> ctx) {
+      ctx.Value += a;
+    }
+  }
 
   [Test]
   public void BasicTest() {
-    var a = 0;
-    ICommandBus cb = new CommandCenter().With<E>(e => a++).With<EE>(() => a++);
+    var a = new Watch<int>(0);
+    ICommandBus<Watch<int>> cb = new CommandBus<Watch<int>>(a);
 
     cb.Push<E>();
-    Assert.AreEqual(1, a);
+    Assert.AreEqual(1, a.Value);
 
-    a = 0;
-    cb.Push(new EE(1));
-    Assert.AreEqual(1, a);
+    a.Value = 0;
+    cb.Push(new EE(2));
+    Assert.AreEqual(2, a.Value);
   }
 
   [Test]
   public void DelayedTest() {
-    var a = 0;
-    var cb = new DelayedCommandCenter();
-    ICommandBus _ = cb;
+    var a = new Watch<int>(0);
+    var cb = new DelayedCommandBus<Watch<int>>(a);
 
-    cb.Add<E>(e => a++);
-    cb.Push<E>();
-    Assert.AreEqual(0, a);
+    (cb as ICommandBus<Watch<int>>).Push<E>();
+    Assert.AreEqual(0, a.Value);
 
     cb.Execute();
-    Assert.AreEqual(1, a);
+    Assert.AreEqual(1, a.Value);
   }
 }
